b51eba02dfe3ad72ea8d1fff4d71e5ebab5c7309
Vital Kudzelka <vital.kudzelka@gmail.com>
2015-11-04 09:48:28 +0300

    Set package status to beta

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     keywords='flask humanize datetime',
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Plugins',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
